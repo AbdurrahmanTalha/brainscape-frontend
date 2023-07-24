0de073f026178b86b5d01bc511293ceaae6c3120
MD Anik <122194751+sranik28@users.noreply.github.com>
2023-07-25 02:23:45 +0600

    problem sloved-NavBar

---
+++ app/student/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import DashboardNav from "../components/ui/DashboardNav";
+
+
+const StudentDashboardPage = () => {
+    return (
+        <main>
+            <DashboardNav />
+        </main>
+    );
+};
+
+export default StudentDashboardPage;
