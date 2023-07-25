40460d3a11eb9405c2d2d4805723929c87bcaba5
nicchy123 <nasirchy252@gmail.com>
2023-07-25 19:40:13 +0600

    input field changed

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -12,14 +12,14 @@ const page = () => {
             <div className="lg:w-1/2 flex lg:flex-row flex-col justify-center items-center  bg-[#171320]">
                 <div className="w-4/5 mx-auto py-10">
                     <h3 className="text-4xl font-bold text-center mb-12 text-white">Register</h3>
-                    <div className="flex lg:flex-row flex-col justify-between lg:gap-9">
+                    <div className="flex lg:flex-row flex-col justify-between ">
                         <div>
                             <div className=" w-full mb-7">
                                 <p className="font-medium mb-1 text-white ">First Name</p>
                                 <input
                                     type="text"
                                     placeholder="First Name"
-                                    className="input rounded-md border-2 border-[#9D77EE]  w-full text-xs font-medium "
+                                    className="input rounded-md border-2 border-[#9D77EE] lg:w-[90%]  w-full text-xs font-medium "
                                 />
                             </div>
                             <div className=" w-full mb-7">
@@ -27,7 +27,7 @@ const page = () => {
                                 <input
                                     type="text"
                                     placeholder="Last Name"
-                                    className="input rounded-md border-2 border-[#9D77EE]  w-full text-xs font-medium "
+                                    className="input rounded-md border-2 border-[#9D77EE]  lg:w-[90%]  w-full text-xs font-medium "
                                 />
                             </div>
                             <div className=" w-full mb-7">
@@ -35,7 +35,7 @@ const page = () => {
                                 <input
                                     type="text"
                                     placeholder="Email"
-                                    className="input rounded-md border-2 border-[#9D77EE]  w-full text-xs font-medium"
+                                    className="input rounded-md border-2 border-[#9D77EE] lg:w-[90%]  w-full text-xs font-medium"
                                 />
                             </div>
                         </div>
@@ -45,14 +45,14 @@ const page = () => {
                                 <input
                                     type="text"
                                     placeholder="Middle Name"
-                                    className="input rounded-md border-2 border-[#9D77EE]  w-full text-xs font-medium"
+                                    className="input rounded-md border-2 border-[#9D77EE] lg:w-[90%]  w-full text-xs font-medium"
                                 />
                             </div>
                             <div className=" w-4/5 mt-7">
                                 <p className="font-medium mb-1 text-white text-sm">Upload Profile picture</p>
                                 <label className="custom-file-input-label">
                                     <input type="file" className=" hidden"  />
-                                    <div className="flex justify-start gap-2 items-center px-4 py-2 cursor-pointer input rounded-md border-2 border-[#9D77EE] text-xs font-medium">
+                                    <div className="flex justify-start gap-2 items-center lg:w-[80%]  w-full px-4 py-2 cursor-pointer input rounded-md border-2 border-[#9D77EE] text-xs font-medium">
                                       
                                       <FontAwesomeIcon  className="text-white w-5 h-5" icon={faUpload} />
                                       Upload Profile
@@ -64,7 +64,7 @@ const page = () => {
                                 <input
                                     type="password"
                                     placeholder="Password"
-                                    className="input rounded-md border-2 border-[#9D77EE]  w-full text-xs font-medium"
+                                    className="input rounded-md border-2 border-[#9D77EE]  lg:w-[90%]  w-full text-xs font-medium"
                                 />
                                 <p className="mt-2 text-sm text-end cursor-pointer">Already Have an account?</p>
                             </div>
