6b9df2cb2b6132092e20ac23477147fbb9764e5e
AbdurrahmanTalha <abdurrahmantalha.dev@gmail.com>
2023-07-25 20:39:31 +0600

    Save

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,14 +1,14 @@
 import Image from "next/image";
 import React from "react";
-import img from '/public/men.jpg'
+import img from "/public/men.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUpload } from "@fortawesome/free-solid-svg-icons";
 // Test
 const page = () => {
     return (
         <section className="flex lg:flex-row flex-col h-[608px] lg:w-[1218px] w-4/5 mx-auto mt-16 lg:gap-0 gap-5">
-            <div className='lg:w-1/2'>
-                <Image src={img} width={800} height={1600} alt='talha'/>
+            <div className="lg:w-1/2">
+                <Image src={img} width={800} height={1600} alt="talha" />
             </div>
             <div className="lg:w-1/2 flex lg:flex-row flex-col justify-center items-center  bg-[#171320]">
                 <div className="w-4/5 mx-auto py-10">
@@ -40,7 +40,7 @@ const page = () => {
                                 />
                             </div>
                         </div>
-                        <div  className="w-full">
+                        <div className="w-full">
                             <div className=" w-full ">
                                 <p className="font-medium mb-1 text-white">Middle Name</p>
                                 <input
@@ -52,12 +52,11 @@ const page = () => {
                             <div className=" w-4/5 mt-7">
                                 <p className="font-medium mb-1 text-white text-sm">Upload Profile picture</p>
                                 <label className="custom-file-input-label">
-                                    <input type="file" className=" hidden"  />
+                                    <input type="file" className=" hidden" />
                                     <div className="flex justify-start gap-2 items-center  w-full px-4 py-2 cursor-pointer input rounded-md border-2 border-[#9D77EE] text-xs font-medium">
-                                      
-                                      <FontAwesomeIcon  className="text-white w-5 h-5" icon={faUpload} />
-                                      Upload Profile
-                                      </div>
+                                        <FontAwesomeIcon className="text-white w-5 h-5" icon={faUpload} />
+                                        Upload Profile
+                                    </div>
                                 </label>
                             </div>
                             <div className=" w-full mt-7">
