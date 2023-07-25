41151d38596becd65a96a769857fb85ea3c58767
jakariachynabil10 <jakariachynabil@gmail.com>
2023-07-25 19:49:33 +0600

    hero2 section done

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import Banner from "./Components/Banner/Banner";
+import Hero2 from "./Components/Hero2/Hero2";
 import Statistic from "./Components/Statistic/Statistic";
-
 
 export default function Home() {
     return (
         <main>
             <Banner></Banner>
             <Statistic></Statistic>
+            <Hero2></Hero2>
         </main>
     );
 }
