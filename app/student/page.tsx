51ffcf9a3557fd482a7fa6d99346b79b6583eef7
MD Anik <122194751+sranik28@users.noreply.github.com>
2023-07-25 16:44:23 +0600

    Pick up section done

--- app/student/page.tsx
+++ app/student/page.tsx
@@ -3,6 +3,8 @@ import DashboardNav from "../components/ui/DashboardNav";
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import CLearningTable from "../components/ui/CLearningTable";
+import img from "../../public/Rectangle.png"
 
 
 const StudentDashboardPage = () => {
@@ -11,7 +13,7 @@ const StudentDashboardPage = () => {
             <DashboardNav />
             <section className="my-20 ">
                 <div className="grid md:w-[1380px] gap-5 md:h-[176px] md:grid-cols-2">
-                    
+
                     <div className="flex md:w-[558px] md:h-[160px] items-center gap-8  ">
                         <Image className="md:w-[160px] rounded-full object-cover md:h-[160px]" width={100} height={100} src="https://i.ibb.co/vsC46pf/bw.jpg" alt="" />
                         <div>
@@ -78,13 +80,22 @@ const StudentDashboardPage = () => {
                     <div>
                         <h2 className="my-3 text-[24px] font-semibold text-[#FFFFFF]">Pick up where you left of!</h2>
                         <div className="border rounded-md md:h-[509px] border-[#9D77EE] md:w-[600px]">
+                            <Image className="w-full object-cover h-[280px]" width={100} height={100} src={img} alt="" />
+                            <h2 className="text-[24px] font-semibold text-[#FFFFFF] mt-5 ml-5">Mathmatics</h2>
+                            <div className="flex items-center gap-10 mt-6 ml-8">
+                                <div className="radial-progress " style={{ "--value ": 55    }}>55%</div>
+                                <div>
+                                    <h2 className="text-[#FFFFFF] font-semibold text-[24px]">30 Quzzies out of 40</h2>
+                                    <button className="bg-[#171320] px-8 mt-3 py-2 text-[#FFFFFF] rounded-md">Continue lesson</button>
+                                </div>
 
+                            </div>
                         </div>
                     </div>
                     <div>
                         <h2 className="my-3 text-[24px] font-semibold text-[#FFFFFF]">Continue Learning</h2>
                         <div className="border rounded-md md:h-[509px] border-[#9D77EE] md:w-[600px]">
-
+                            <CLearningTable />
                         </div>
                     </div>
                 </div>
@@ -95,17 +106,17 @@ const StudentDashboardPage = () => {
             <section className="mt-10 ">
                 <h2 className="mb-3 text-[24px] font-semibold text-[#FFFFFF]">Recommended Courses</h2>
                 <div className="grid gap-5 mb-5 md:grid-cols-3">
-                    <div className=" hover:scale-95 transform duration-500 p-10 md:w-[410px] md:h-[380px] border border-[#9D77EE] ">
+                    <div className=" hover:scale-95 transform duration-500 p-10 md:w-[410px] rounded-md md:h-[380px] border border-[#9D77EE] ">
                         <Image className="block object-cover w-40 h-40 mx-auto rounded-full " src="https://i.ibb.co/vsC46pf/bw.jpg" alt="" width={100} height={100} />
                         <p className="text-[#FFFFFF] mt-10 mb-3">30 Quzzes - Something</p>
                         <p className="text-[24px] font-bold text-[#FFFFFF]">Logic</p>
                     </div>
-                    <div className=" hover:scale-95 transform duration-500 p-10 md:w-[410px] md:h-[380px] border border-[#9D77EE] ">
+                    <div className=" hover:scale-95 transform duration-500 p-10 md:w-[410px] rounded-md md:h-[380px] border border-[#9D77EE] ">
                         <Image className="block object-cover w-40 h-40 mx-auto rounded-full h" src="https://i.ibb.co/vsC46pf/bw.jpg" alt="" width={100} height={100} />
                         <p className="text-[#FFFFFF] mt-10 mb-3">30 Quzzes - Something</p>
                         <p className="text-[24px] font-bold text-[#FFFFFF]">Logic</p>
                     </div>
-                    <div className=" hover:scale-95 transform duration-500 p-10 md:w-[410px] md:h-[380px] border border-[#9D77EE] ">
+                    <div className=" hover:scale-95 transform duration-500 p-10 md:w-[410px] rounded-md md:h-[380px] border border-[#9D77EE] ">
                         <Image className="block object-cover w-40 h-40 mx-auto rounded-full h" src="https://i.ibb.co/vsC46pf/bw.jpg" alt="" width={100} height={100} />
                         <p className="text-[#FFFFFF] mt-10 mb-3">30 Quzzes - Something</p>
                         <p className="text-[24px] font-bold text-[#FFFFFF]">Logic</p>
