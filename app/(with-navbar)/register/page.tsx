5f3bb9283f5c22296ae9a3153a1978448eee29ee
MD Anik <122194751+sranik28@users.noreply.github.com>
2023-09-10 22:52:44 +0600

    register pasge

--- app/(with-navbar)/register/page.tsx
+++ app/(with-navbar)/register/page.tsx
@@ -4,11 +4,18 @@ import React from "react";
 import img from "/public/men.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUpload } from "@fortawesome/free-solid-svg-icons";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { setCredentials } from "@/app/redux/feature/authSlice";
+import { useAppDispatch } from "@/app/redux/hook";
 // Test
-const page = () => {
-    const onSubmit = (e: { preventDefault: () => void }) => {
-        e.preventDefault();
-        console.log("submit");
+const RegisterPage = () => {
+    const dispatch = useAppDispatch();
+
+    const { register, handleSubmit } = useForm<Inputs>();
+
+    const onSubmit: SubmitHandler<Inputs> = data => {
+        dispatch(setCredentials(data));
+        console.log(data);
     };
 
     return (
@@ -17,13 +24,14 @@ const page = () => {
                 <Image src={img} width={800} height={1600} alt="talha" />
             </div>
             <div className="lg:w-1/2 flex lg:flex-row flex-col justify-center items-center  bg-[#171320]">
-                <form onSubmit={onSubmit} className="w-4/5 py-10 mx-auto">
+                <form onSubmit={handleSubmit(onSubmit)} className="w-4/5 py-10 mx-auto">
                     <h3 className="mb-12 text-4xl font-bold text-center text-white">Register</h3>
                     <div className="flex flex-col justify-between lg:flex-row gap-7">
                         <div className="w-full">
                             <div className="w-full mb-7">
                                 <p className="mb-1 font-medium text-white ">First Name</p>
                                 <input
+                                    {...register("firstName")}
                                     type="text"
                                     name="firstName"
                                     placeholder="First Name"
@@ -33,6 +41,7 @@ const page = () => {
                             <div className="w-full mb-7">
                                 <p className="mb-1 font-medium text-white ">Last Name</p>
                                 <input
+                                    {...register("lastName")}
                                     type="text"
                                     name="lastName"
                                     placeholder="Last Name"
@@ -42,6 +51,7 @@ const page = () => {
                             <div className="w-full mb-7">
                                 <p className="mb-1 font-medium text-white ">Email</p>
                                 <input
+                                    {...register("email")}
                                     type="text"
                                     name="email"
                                     placeholder="Email"
@@ -53,6 +63,7 @@ const page = () => {
                             <div className="w-full ">
                                 <p className="mb-1 font-medium text-white">Middle Name</p>
                                 <input
+                                    {...register("middleName")}
                                     type="text"
                                     name="middleName"
                                     placeholder="Middle Name"
@@ -62,7 +73,12 @@ const page = () => {
                             <div className="w-4/5 mt-7">
                                 <p className="mb-1 text-sm font-medium text-white">Upload Profile picture</p>
                                 <label className="custom-file-input-label">
-                                    <input type="file" name="profileImg" className="hidden " />
+                                    <input
+                                        {...register("profileImg")}
+                                        type="file"
+                                        name="profileImg"
+                                        className="hidden "
+                                    />
                                     <div className="flex justify-start gap-2 items-center  w-full px-4 py-2 cursor-pointer input rounded-md border-2 border-[#9D77EE] text-xs font-medium">
                                         <FontAwesomeIcon className="w-5 h-5 text-white" icon={faUpload} />
                                         Upload Profile
@@ -72,6 +88,7 @@ const page = () => {
                             <div className="w-full mt-7">
                                 <p className="mb-1 font-medium text-white">Password</p>
                                 <input
+                                    {...register("password")}
                                     type="password"
                                     name="password"
                                     placeholder="Password"
@@ -94,4 +111,4 @@ const page = () => {
     );
 };
 
-export default page;
+export default RegisterPage;
